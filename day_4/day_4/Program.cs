8be586105fed8d41072a8e782fc1d96423f1ac4d
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-09 19:21:21 +0300

    pirmais

--- day_4/day_4/Program.cs
+++ day_4/day_4/Program.cs
@@ -6,7 +6,26 @@ namespace day_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mainigais = 3;
+
+            // <
+            // >
+            // == 
+            // !=
+            // <=
+            // >=
+
+            if (mainigais < 5)
+            {
+                Console.WriteLine("mazaks");
+            }
+
+            else
+            {
+                Console.WriteLine("lielaks");
+            }
+            Console.ReadLine();
+
         }
     }
 }
