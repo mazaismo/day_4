a8d5c09053a4d91adcafa864963ba752e3ed0e77
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-09 21:25:41 +0300

    us presidents

--- day_4/day_4/Program.cs
+++ day_4/day_4/Program.cs
@@ -38,7 +38,7 @@ namespace day_4
                  Console.WriteLine("nav vienads");
              }
              */
-            
+            /*
             string skaitlis;
             int skaitlis1 = 2;
             skaitlis = Console.ReadLine();
@@ -56,42 +56,42 @@ namespace day_4
             Console.ReadLine();
             */
 
-           /* string skaitlis;
-            int skaitlis1 = 2;
-            skaitlis = Console.ReadLine();
-            skaitlis1 = Convert.ToInt16(skaitlis);
+            /* string skaitlis;
+             int skaitlis1 = 2;
+             skaitlis = Console.ReadLine();
+             skaitlis1 = Convert.ToInt16(skaitlis);
 
-            if(skaitlis1 == 4)
-            {
-                Console.WriteLine("četrstūris");
-            }
-            else
-            {
-                if (skaitlis1 == 3)
-                {
-                    Console.WriteLine("trijstūris");
-                }
-                else
-                {
-                    if (skaitlis1 == 6)
-                    {
-                        Console.WriteLine("sešturis");
-                    }
-                    else
-                    {
-                        if (skaitlis1 == 5)
-                        {
-                            Console.WriteLine("piecstūris");
-                        }
-                        else
-                        {
-                            Console.WriteLine("out of range");
-                        }
-                    }
-                }
-            }
-            */
-
+             if(skaitlis1 == 4)
+             {
+                 Console.WriteLine("četrstūris");
+             }
+             else
+             {
+                 if (skaitlis1 == 3)
+                 {
+                     Console.WriteLine("trijstūris");
+                 }
+                 else
+                 {
+                     if (skaitlis1 == 6)
+                     {
+                         Console.WriteLine("sešturis");
+                     }
+                     else
+                     {
+                         if (skaitlis1 == 5)
+                         {
+                             Console.WriteLine("piecstūris");
+                         }
+                         else
+                         {
+                             Console.WriteLine("out of range");
+                         }
+                     }
+                 }
+             }
+             */
+            /*
             switch (skaitlis1)
             {
                 case 3:
@@ -110,9 +110,54 @@ namespace day_4
                     Console.WriteLine("kluda!");
                     break;
             }
+            */
+
+            Console.WriteLine("please enter value 1,2,5,10,20,50,100");
+            string skaitlis;
+            int skaitlis1 = 2;
+            skaitlis = Console.ReadLine();
+            skaitlis1 = Convert.ToInt16(skaitlis);
+
+            
+
+            switch (skaitlis1)
+            {
+
+                case 1:
+                    Console.WriteLine("George Washington");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Thomas Jefferson");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Abraham Lincoln");
+                    break;
+
+                case 10:
+                    Console.WriteLine("Alexander Hamilton");
+                    break;
+
+                case 20:
+                    Console.WriteLine("Andrew Jackson");
+                    break;
+
+                case 50:
+                    Console.WriteLine("Ulysses S.Grant");
+                    break;
+
+                case 100:
+                    Console.WriteLine("Benjamin Franklin");
+                    break;
+
+                default:
+                    Console.WriteLine("You entered a wrong value");
+                    break;
+            }
 
 
-           Console.ReadLine();
+            Console.ReadLine();
         }
 
     }
